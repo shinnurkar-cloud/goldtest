be7fb9b3a1e0770b47c6f15743a015a07969a4c4
shinnurkar-cloud <shinnurkar@gmail.com>
2025-08-22 15:31:25 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,25 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
